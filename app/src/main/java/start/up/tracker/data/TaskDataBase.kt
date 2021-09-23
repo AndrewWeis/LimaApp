4981ab5507b1a379720380b363bc23ea3ea96664
Andrew Weiss <andreivayss@gmail.com>
2021-09-23 22:37:16 +0700

    Added UserDataBase

---
+++ app/src/main/java/start/up/tracker/data/TaskDataBase.kt
@@ -0,0 +1,36 @@
+package start.up.tracker.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import start.up.tracker.model.Task
+
+
+@Database(entities = [Task::class], version = 1, exportSchema = false)
+abstract class TaskDataBase: RoomDatabase() {
+
+    abstract fun userDao(): TaskDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TaskDataBase? = null
+
+        fun getDatabase(context: Context): TaskDataBase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TaskDataBase::class.java,
+                    "task_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
