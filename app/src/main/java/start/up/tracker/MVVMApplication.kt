4da009dc34145b7e9bf1a3a54f67a723f7d20b59
Andrew Weiss <andreivayss@gmail.com>
2021-09-25 16:44:52 +0700

    Added layouts

---
+++ app/src/main/java/start/up/tracker/MVVMApplication.kt
@@ -0,0 +1,7 @@
+package start.up.tracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MVVMApplication : Application()
---
+++ app/src/main/java/start/up/tracker/ui/AddTaskActivity.kt
@@ -0,0 +1,4 @@
+package start.up.tracker.ui
+
+class AddTaskActivity {
+}
