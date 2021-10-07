1b1172df3b6ec302e4a8e26c0d57162c0368bea1
Andrew Weiss <andreivayss@gmail.com>
2021-10-07 09:20:37 +0700

    Added radio group for priority

---
+++ Android/Lima/app/src/main/java/start/up/tracker/ui/RadioGridGroup.kt
@@ -0,0 +1,117 @@
+package start.up.tracker.ui
+
+import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
+import android.os.Parcelable.Creator
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.TableLayout
+import android.widget.TableRow
+import androidx.annotation.IdRes
+
+class RadioGridGroup : TableLayout, View.OnClickListener {
+    private var checkedRadioButtonId = -1
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    override fun onClick(v: View) {
+        if (v is RadioButton) {
+            val id = v.getId()
+            check(id)
+        }
+    }
+
+    private fun setCheckedStateForView(viewId: Int, checked: Boolean) {
+        val checkedView = findViewById<View>(viewId)
+        if (checkedView != null && checkedView is RadioButton) {
+            checkedView.isChecked = checked
+        }
+    }
+
+    override fun addView(
+        child: View, index: Int,
+        params: ViewGroup.LayoutParams
+    ) {
+        super.addView(child, index, params)
+        setChildrenOnClickListener(child as TableRow)
+    }
+
+    override fun addView(child: View, params: ViewGroup.LayoutParams) {
+        super.addView(child, params)
+        setChildrenOnClickListener(child as TableRow)
+    }
+
+    private fun setChildrenOnClickListener(tr: TableRow) {
+        val c = tr.childCount
+        for (i in 0 until c) {
+            val v = tr.getChildAt(i)
+            (v as? RadioButton)?.setOnClickListener(this)
+        }
+    }
+
+    private fun check(@IdRes id: Int) {
+        if (id != -1 && id == checkedRadioButtonId) {
+            return
+        }
+        if (checkedRadioButtonId != -1) {
+            setCheckedStateForView(checkedRadioButtonId, false)
+        }
+        if (id != -1) {
+            setCheckedStateForView(id, true)
+        }
+        setCheckedId(id)
+    }
+
+    private fun setCheckedId(id: Int) {
+        checkedRadioButtonId = id
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable) {
+        if (state !is SavedState) {
+            super.onRestoreInstanceState(state)
+            return
+        }
+        super.onRestoreInstanceState(state.superState)
+        checkedRadioButtonId = state.buttonId
+        setCheckedStateForView(checkedRadioButtonId, true)
+    }
+
+    override fun onSaveInstanceState(): Parcelable {
+        val superState = super.onSaveInstanceState()
+        val savedState = SavedState(superState)
+        savedState.buttonId = checkedRadioButtonId
+        return savedState
+    }
+
+    internal class SavedState : BaseSavedState {
+        var buttonId = 0
+
+        constructor(source: Parcel) : super(source) {
+            buttonId = source.readInt()
+        }
+
+        constructor(superState: Parcelable?) : super(superState)
+
+        override fun writeToParcel(out: Parcel, flags: Int) {
+            super.writeToParcel(out, flags)
+            out.writeInt(buttonId)
+        }
+
+        companion object {
+            @JvmField
+            val CREATOR: Creator<SavedState?> = object : Creator<SavedState?> {
+                override fun createFromParcel(`in`: Parcel): SavedState {
+                    return SavedState(`in`)
+                }
+
+                override fun newArray(size: Int): Array<SavedState?> {
+                    return arrayOfNulls(size)
+                }
+            }
+        }
+    }
+}
