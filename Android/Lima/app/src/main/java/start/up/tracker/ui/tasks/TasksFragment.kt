aa18e0c888ba65bf11c8117751b7c0d694bd14c5
Andrew Weiss <andreivayss@gmail.com>
2021-10-16 11:20:53 +0700

    Implemented Delete-All-Completed Dialog

--- Android/Lima/app/src/main/java/start/up/tracker/ui/tasks/TasksFragment.kt
+++ Android/Lima/app/src/main/java/start/up/tracker/ui/tasks/TasksFragment.kt
@@ -95,6 +95,10 @@ class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.OnItemClic
                     is TasksViewModel.TasksEvent.ShowTaskSavedConfirmationMessage -> {
                         Snackbar.make(requireView(), event.msg, Snackbar.LENGTH_SHORT).show()
                     }
+                    is TasksViewModel.TasksEvent.NavigateToDeleteAllCompletedScreen -> {
+                        val action = TasksFragmentDirections.actionGlobalDeleteAllCompletedDialogFragment()
+                        findNavController().navigate(action)
+                    }
                 }.exhaustive
             }
         }
@@ -142,6 +146,7 @@ class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.OnItemClic
                 true
             }
             R.id.action_delete_all_completed_tasks -> {
+                viewModel.onDeleteAllCompletedClick()
                 true
             }
             else -> super.onOptionsItemSelected(item)
