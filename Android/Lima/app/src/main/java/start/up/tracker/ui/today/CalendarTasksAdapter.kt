67b07e3bdcfada60448dafbd8ad6b456acdc1ccb
Andrew Weiss <andreivayss@gmail.com>
2021-12-06 10:30:20 +0700

    Implemented the display of tasks in a calendar way

--- Android/Lima/app/src/main/java/start/up/tracker/ui/today/CalendarTasksAdapter.kt
+++ Android/Lima/app/src/main/java/start/up/tracker/ui/today/CalendarTasksAdapter.kt
@@ -1,6 +1,9 @@
 package start.up.tracker.ui.today
 
+import android.content.Context
+import android.content.res.Resources
 import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,7 +25,8 @@ class CalendarTasksAdapter(private val listener: OnItemClickListener) : ListAdap
 
     override fun onBindViewHolder(holder: TasksViewHolder, position: Int) {
         val currentItem = getItem(position)
-        holder.bind(currentItem)
+        val nextItem: TodayTask? = if (position + 1 < itemCount) { getItem(position + 1) } else { null }
+        holder.bind(currentItem, nextItem)
     }
 
     inner class TasksViewHolder(private val binding: ItemTaskCalendarBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -47,20 +51,31 @@ class CalendarTasksAdapter(private val listener: OnItemClickListener) : ListAdap
             }
         }
 
-        fun bind(todayTask: TodayTask) {
+        fun bind(todayTask: TodayTask, nextTask: TodayTask?) {
             binding.apply {
                 checkBoxCompleted.isChecked = todayTask.completed
                 textViewName.text = todayTask.taskName
                 textViewName.paint.isStrikeThruText = todayTask.completed
-
                 if (todayTask.priority == 4) {
                     icPriority.visibility = View.GONE
                 } else {
                     val color = chooseColorPriority(todayTask.priority)
                     icPriority.setBackgroundColor(Color.parseColor(color))
                 }
+
+                val layoutParams: ViewGroup.MarginLayoutParams = binding.cardTaskCalendar.layoutParams as ViewGroup.MarginLayoutParams
+                var space = 1
+                if (nextTask != null) {
+                    space = (nextTask.timeStartInt - todayTask.timeEndInt) * 30
+                }
+                layoutParams.bottomMargin = convertDpToPx(space)
+                val height = (todayTask.timeEndInt - todayTask.timeStartInt) * 30 - 1
+                layoutParams.height = convertDpToPx(height)
+                binding.cardTaskCalendar.requestLayout()
             }
         }
+
+        private fun convertDpToPx(dp: Int) = (dp * Resources.getSystem().displayMetrics.density).toInt()
 
         private fun chooseColorPriority(priority: Int): String {
             return when(priority) {
