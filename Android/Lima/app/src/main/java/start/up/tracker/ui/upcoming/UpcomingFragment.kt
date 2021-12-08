f3540e64faca9bc1f08345c89e78ae9a672b6486
BIGNIKi <nikita_sr1@mail.ru>
2021-12-08 17:20:01 +0700

    Implemented getting upcoming tasks in ViewModel

--- Android/Lima/app/src/main/java/start/up/tracker/ui/upcoming/UpcomingFragment.kt
+++ Android/Lima/app/src/main/java/start/up/tracker/ui/upcoming/UpcomingFragment.kt
@@ -1,9 +1,26 @@
 package start.up.tracker.ui.upcoming
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import start.up.tracker.R
+import start.up.tracker.databinding.FragmentUpcomingBinding
+import start.up.tracker.ui.today.TodayViewModel
 
 @AndroidEntryPoint
-class UpcomingFragment : Fragment() {
+class UpcomingFragment : Fragment(R.layout.fragment_upcoming) {
 
+    private val viewModel: UpcomingViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentUpcomingBinding.bind(view)
+
+        viewModel.upcomingTasks.observe(viewLifecycleOwner) {
+            Log.i("upcoming", it.toString())
+        }
+    }
 }
--- Android/Lima/app/src/main/java/start/up/tracker/ui/upcoming/UpcomingViewModel.kt
+++ Android/Lima/app/src/main/java/start/up/tracker/ui/upcoming/UpcomingViewModel.kt
@@ -1,13 +1,27 @@
 package start.up.tracker.ui.upcoming
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.flatMapLatest
+import start.up.tracker.data.db.PreferencesManager
 import start.up.tracker.data.db.TaskDao
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class UpcomingViewModel @Inject constructor(
     private val taskDao: TaskDao,
+    private val preferencesManager: PreferencesManager
 ) : ViewModel() {
+
+    val hideCompleted = preferencesManager.hideCompleted
+    private val currentDate = Date().time
+
+
+    private val upcomingTasksFlow = hideCompleted.flatMapLatest {
+        taskDao.getUpcomingTasks(currentDate, it ?: false)
+    }
+    val upcomingTasks = upcomingTasksFlow.asLiveData()
 
 }
