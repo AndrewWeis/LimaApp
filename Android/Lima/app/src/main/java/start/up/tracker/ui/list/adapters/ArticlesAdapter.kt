057deef2f6132855f0f19438d52154fc7ff57a59
Andrew Weiss <andreivayss@gmail.com>
2022-02-23 17:21:02 +0700

    Implemented ArticlesAdapter.kt for articles

--- Android/Lima/app/src/main/java/start/up/tracker/ui/list/adapters/ArticlesAdapter.kt
+++ Android/Lima/app/src/main/java/start/up/tracker/ui/list/adapters/ArticlesAdapter.kt
@@ -1,4 +1,51 @@
 package start.up.tracker.ui.list.adapters
 
-class ArticlesAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import start.up.tracker.data.entities.Article
+import start.up.tracker.databinding.ItemArticleBinding
+
+class ArticlesAdapter(
+    private val listener: OnArticleClickListener
+) : ListAdapter<Article, ArticlesAdapter.ArticlesViewHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticlesViewHolder {
+        val binding = ItemArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ArticlesViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ArticlesViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    inner class ArticlesViewHolder(
+        private val binding: ItemArticleBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(article: Article) {
+            binding.articleTitleText.text = article.title
+            binding.articleDescriptionText.text = article.body
+            binding.articleReadTimeText.text = article.timeToRead
+
+            binding.root.setOnClickListener {
+                listener.onArticleClick(article)
+            }
+        }
+    }
+
+    interface OnArticleClickListener {
+        fun onArticleClick(article: Article)
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article) =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article) =
+            oldItem == newItem
+    }
 }
