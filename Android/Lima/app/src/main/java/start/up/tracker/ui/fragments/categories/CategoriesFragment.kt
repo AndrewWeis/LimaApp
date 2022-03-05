7ddc24f474a10002f527b1b359c48950bdc2d02c
Andrew Weiss <andreivayss@gmail.com>
2022-03-05 20:08:20 +0700

    Refactored CategoriesFragment.kt into a readable view

--- Android/Lima/app/src/main/java/start/up/tracker/ui/fragments/categories/CategoriesFragment.kt
+++ Android/Lima/app/src/main/java/start/up/tracker/ui/fragments/categories/CategoriesFragment.kt
@@ -21,92 +21,112 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 @AndroidEntryPoint
-class CategoriesFragment: Fragment(R.layout.fragment_categories), CategoriesAdapter.OnItemClickListener{
+class CategoriesFragment :
+    Fragment(R.layout.fragment_categories),
+    CategoriesAdapter.OnItemClickListener {
 
     private val viewModel: CategoriesViewModel by viewModels()
 
+    private var binding: FragmentCategoriesBinding? = null
+    private lateinit var categoryAdapter: CategoriesAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentCategoriesBinding.bind(view)
 
-        val binding = FragmentCategoriesBinding.bind(view)
+        initAdapter()
+        initListeners()
+        initTaskEventListener()
 
-        val categoryAdapter = CategoriesAdapter(this)
+        viewModel.updateNumberOfTasks()
+    }
 
-        binding.apply {
-            categoryRV.apply {
-                adapter = categoryAdapter
-                layoutManager = LinearLayoutManager(requireContext())
-                setHasFixedSize(true)
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+    override fun onItemClick(category: Category) {
+        viewModel.onCategorySelected(category)
+    }
+
+    private fun initTaskEventListener() = viewLifecycleOwner.lifecycleScope.launchWhenCreated {
+        viewModel.categoryEvent.collect { event ->
+            when (event) {
+                is CategoriesViewModel.CategoryEvent.NavigateToCategoryInside -> {
+                    val action = CategoriesFragmentDirections.actionCategoryToCategoryInside(
+                        event.category,
+                        event.category.categoryId,
+                        event.category.categoryName
+                    )
+                    findNavController().navigate(action)
+                }
+                CategoriesViewModel.CategoryEvent.NavigateToAddCategoryScreen -> {
+                    val action = CategoriesFragmentDirections.actionCategoryToAddCategory()
+                    findNavController().navigate(action)
+                }
+                is CategoriesViewModel.CategoryEvent.ShowCategorySavedConfirmationMessage -> {
+                    Snackbar.make(requireView(), event.msg, Snackbar.LENGTH_SHORT).show()
+                }
+                CategoriesViewModel.CategoryEvent.NavigateToToday -> {
+                    val formatter = SimpleDateFormat("dd MMM, EEEE")
+                    val currentDate: String = formatter.format(Date())
+                    val action = CategoriesFragmentDirections.actionCategoryToToday(currentDate)
+                    findNavController().navigate(action)
+                }
+                CategoriesViewModel.CategoryEvent.NavigateToUpcoming -> {
+                    val action = CategoriesFragmentDirections.actionCategoryToUpcoming()
+                    findNavController().navigate(action)
+                }
+            }.exhaustive
+        }
+    }
+
+    private fun initListeners() {
+        binding?.cardViewInbox?.setOnClickListener {
+            viewModel.onCategoryInboxSelected(Category(categoryName = "Inbox", categoryId = 1))
+        }
+
+        binding?.addCategoryFAB?.setOnClickListener {
+            viewModel.onAddNewCategoryClick()
+        }
+
+        binding?.cardViewToday?.setOnClickListener {
+            viewModel.onTodaySelected()
+        }
+
+        binding?.cardViewUpcoming?.setOnClickListener {
+            viewModel.onUpcomingSelected()
         }
 
         viewModel.categories.observe(viewLifecycleOwner) {
             categoryAdapter.submitList(it)
         }
 
-        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
-            viewModel.categoryEvent.collect { event ->
-                when(event) {
-                    is CategoriesViewModel.CategoryEvent.NavigateToCategoryInside -> {
-                        val action = CategoriesFragmentDirections.actionCategoryToCategoryInside(event.category, event.category.categoryId, event.category.categoryName)
-                        findNavController().navigate(action)
-                    }
-                    CategoriesViewModel.CategoryEvent.NavigateToAddCategoryScreen -> {
-                        val action = CategoriesFragmentDirections.actionCategoryToAddCategory()
-                        findNavController().navigate(action)
-                    }
-                    is CategoriesViewModel.CategoryEvent.ShowCategorySavedConfirmationMessage -> {
-                        Snackbar.make(requireView(), event.msg, Snackbar.LENGTH_SHORT).show()
-                    }
-                    CategoriesViewModel.CategoryEvent.NavigateToToday -> {
-                        val formatter = SimpleDateFormat("dd MMM, EEEE")
-                        val currentDate: String = formatter.format(Date())
-                        val action = CategoriesFragmentDirections.actionCategoryToToday(currentDate)
-                        findNavController().navigate(action)
-                    }
-                    CategoriesViewModel.CategoryEvent.NavigateToUpcoming -> {
-                        val action = CategoriesFragmentDirections.actionCategoryToUpcoming()
-                        findNavController().navigate(action)
-                    }
-                }.exhaustive
-            }
+        viewModel.getInboxTasksCount.observe(viewLifecycleOwner) {
+            binding?.tvInboxNum?.text = it.toString()
         }
 
-        binding.cardViewInbox.setOnClickListener {
-            viewModel.onCategoryInboxSelected(Category(categoryName = "Inbox", categoryId = 1))
+        viewModel.todayTasksCount.observe(viewLifecycleOwner) {
+            binding?.tvTodayNum?.text = it.toString()
         }
 
-        binding.addCategoryFAB.setOnClickListener {
-            viewModel.onAddNewCategoryClick()
+        viewModel.upcomingTasksCount.observe(viewLifecycleOwner) {
+            binding?.tvUpcomingNum?.text = it.toString()
         }
 
         setFragmentResultListener("add_request") { _, _ ->
             viewModel.showCategorySavedConfirmationMessage()
         }
-
-        viewModel.updateNumberOfTasks()
-
-        viewModel.getInboxTasksCount.observe(viewLifecycleOwner) {
-            binding.tvInboxNum.text = it.toString()
-        }
-
-        viewModel.todayTasksCount.observe(viewLifecycleOwner) {
-            binding.tvTodayNum.text = it.toString()
-        }
-
-        viewModel.upcomingTasksCount.observe(viewLifecycleOwner) {
-            binding.tvUpcomingNum.text = it.toString()
-        }
-
-        binding.cardViewToday.setOnClickListener {
-            viewModel.onTodaySelected()
-        }
-        binding.cardViewUpcoming.setOnClickListener {
-            viewModel.onUpcomingSelected()
-        }
     }
 
-    override fun onItemClick(category: Category) {
-        viewModel.onCategorySelected(category)
+    private fun initAdapter() {
+        categoryAdapter = CategoriesAdapter(this)
+
+        binding?.categoryRV?.apply {
+            adapter = categoryAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+        }
     }
 }
