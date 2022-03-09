ebec5170bf3476942410146967a661cba252301b
Andrew Weiss <andreivayss@gmail.com>
2022-03-09 19:34:54 +0700

    Added StatisticsTask.kt for active analytic

---
+++ Android/Lima/app/src/main/java/start/up/tracker/analytics/entities/StatisticsTask.kt
@@ -0,0 +1,19 @@
+package start.up.tracker.analytics.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class StatisticsTask(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val status: TaskStatus,
+    val categoryId: Int,
+    val creationTime: Long,
+    val expectedCompletionTime: Long?,
+    val completionTime: Long,
+)
+
+enum class TaskStatus {
+    IN_PROGRESS, COMPLETED_IN_TIME, COMPLETED_LATE, COMPLETED
+}
