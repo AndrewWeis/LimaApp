0cbc2168a96d5fb2d9e01275cda056e2c18c791e
Ilya Volf <volfilya@gmail.com>
2022-04-20 17:37:10 +0700

    scroll and multiple graphs support

--- Android/Lima/app/src/main/java/start/up/tracker/ui/fragments/analytics/AnalyticsMonthFragment.kt
+++ Android/Lima/app/src/main/java/start/up/tracker/ui/fragments/analytics/AnalyticsMonthFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.anychart.APIlib
 import com.anychart.AnyChart
 import com.anychart.enums.Anchor
 import com.anychart.enums.HoverMode
@@ -35,7 +36,8 @@ class AnalyticsMonthFragment : Fragment(R.layout.fragment_analytics_month) {
 
     private fun initData() {
         binding?.lineChartMonthAllTasks?.setProgressBar(binding!!.progressBar)
-        binding?.lineChartMonthCompletedTasks
+        binding?.lineChartMonthCompletedTasks?.setProgressBar(binding!!.progressBar)
+        binding?.lineChartMonthUncompletedTasks?.setProgressBar(binding!!.progressBar)
     }
 
     private fun initObservers() {
@@ -47,6 +49,7 @@ class AnalyticsMonthFragment : Fragment(R.layout.fragment_analytics_month) {
     }
 
     private fun initTasksChart() {
+        APIlib.getInstance().setActiveAnyChartView(binding?.lineChartMonthAllTasks);
         val chart = AnyChart.column()
         val column = chart.column(viewModel.data)
 
@@ -74,6 +77,65 @@ class AnalyticsMonthFragment : Fragment(R.layout.fragment_analytics_month) {
         chart.interactivity().hoverMode(HoverMode.BY_X)
 
         binding?.lineChartMonthAllTasks?.setChart(chart)
-        //binding?.lineChartMonthCompletedTasks?.setChart(chart)
+
+        APIlib.getInstance().setActiveAnyChartView(binding?.lineChartMonthCompletedTasks);
+        val chart2 = AnyChart.column()
+        val column2 = chart2.column(viewModel.data)
+
+        column2.tooltip()
+            .titleFormat("{%X}")
+            .position(Position.CENTER_BOTTOM)
+            .anchor(Anchor.CENTER_BOTTOM)
+            .offsetX(5.0)
+            .offsetY(5.0)
+            .format("Tasks: {%Value}{groupsSeparator: }");
+
+        chart2.xAxis(0).labels().fontSize(10)
+        chart2.xAxis(0).title(viewModel.currentMonthName)
+
+        chart2.yScale().minimumGap(1)
+        chart2.yAxis(0).labels().fontSize(10)
+
+        chart2.title("Completed tasks")
+        chart2.title().fontSize(12)
+        chart2.title().fontColor("#858585")
+
+        chart2.animation(true)
+
+        chart2.tooltip().positionMode(TooltipPositionMode.POINT)
+        chart2.interactivity().hoverMode(HoverMode.BY_X)
+
+        binding?.lineChartMonthCompletedTasks?.setChart(chart2)
+
+        /////////////////
+
+        APIlib.getInstance().setActiveAnyChartView(binding?.lineChartMonthUncompletedTasks);
+        val chart3 = AnyChart.column()
+        val column3 = chart3.column(viewModel.data)
+
+        column3.tooltip()
+            .titleFormat("{%X}")
+            .position(Position.CENTER_BOTTOM)
+            .anchor(Anchor.CENTER_BOTTOM)
+            .offsetX(5.0)
+            .offsetY(5.0)
+            .format("Tasks: {%Value}{groupsSeparator: }");
+
+        chart3.xAxis(0).labels().fontSize(10)
+        chart3.xAxis(0).title(viewModel.currentMonthName)
+
+        chart3.yScale().minimumGap(1)
+        chart3.yAxis(0).labels().fontSize(10)
+
+        chart3.title("Completed tasks")
+        chart3.title().fontSize(12)
+        chart3.title().fontColor("#858585")
+
+        chart3.animation(true)
+
+        chart3.tooltip().positionMode(TooltipPositionMode.POINT)
+        chart3.interactivity().hoverMode(HoverMode.BY_X)
+
+        binding?.lineChartMonthUncompletedTasks?.setChart(chart3)
     }
 }
