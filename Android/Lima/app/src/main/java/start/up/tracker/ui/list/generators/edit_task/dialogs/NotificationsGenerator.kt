96da48bbe512a574f168a1a114205f9c6a985d8e
i.chiesov <i.chiesov@gmail.com>
2022-04-26 10:06:33 +0700

    Generate notifications corresponding to current types.

--- Android/Lima/app/src/main/java/start/up/tracker/ui/list/generators/edit_task/dialogs/NotificationsGenerator.kt
+++ Android/Lima/app/src/main/java/start/up/tracker/ui/list/generators/edit_task/dialogs/NotificationsGenerator.kt
@@ -1,64 +1,32 @@
 package start.up.tracker.ui.list.generators.edit_task.dialogs
 
 
-import start.up.tracker.R
-import start.up.tracker.entities.NotificationIds
+import start.up.tracker.entities.NotificationType
 import start.up.tracker.ui.data.entities.ListItem
 import start.up.tracker.ui.data.entities.tasks.ChoiceData
 import start.up.tracker.utils.resources.ResourcesUtils
 
 class NotificationsGenerator {
 
-    fun getDefaultNotificationListItems(selectedNotificationId: Int): List<ListItem> {
+    fun getDefaultNotificationListItems(selectedNotificationType: NotificationType): List<ListItem> {
         val listItems: MutableList<ListItem> = mutableListOf()
-        listItems.add(createNotificationListItem(selectedId = selectedNotificationId, notificationId = NotificationIds.NO_NOTIFICATION))
-        listItems.add(createNotificationListItem(selectedId = selectedNotificationId, notificationId = NotificationIds.AT_TASK_TIME_NOTIFICATION))
-        listItems.add(createNotificationListItem(selectedId = selectedNotificationId, notificationId = NotificationIds.FIVE_MINUTES_BEFORE_NOTIFICATION))
-        listItems.add(createNotificationListItem(selectedId = selectedNotificationId, notificationId = NotificationIds.TEN_MINUTES_BEFORE_NOTIFICATION))
-        listItems.add(createNotificationListItem(selectedId = selectedNotificationId, notificationId = NotificationIds.FIFTEEN_MINUTES_BEFORE_NOTIFICATION))
-        listItems.add(createNotificationListItem(selectedId = selectedNotificationId, notificationId = NotificationIds.THIRTY_MINUTES_BEFORE_NOTIFICATION))
-        listItems.add(createNotificationListItem(selectedId = selectedNotificationId, notificationId = NotificationIds.ONE_HOUR_BEFORE_NOTIFICATION))
-        listItems.add(createNotificationListItem(selectedId = selectedNotificationId, notificationId = NotificationIds.TWO_HOURS_BEFORE_NOTIFICATION))
-        listItems.add(createNotificationListItem(selectedId = selectedNotificationId, notificationId = NotificationIds.ONE_DAY_BEFORE_NOTIFICATION))
+        listItems.add(createNotificationListItem(selectedType = selectedNotificationType, requiredType = NotificationType.NONE))
+        listItems.add(createNotificationListItem(selectedType = selectedNotificationType, requiredType = NotificationType.AT_TASK_TIME))
+        listItems.add(createNotificationListItem(selectedType = selectedNotificationType, requiredType = NotificationType.FIVE_MINUTES_BEFORE))
+        listItems.add(createNotificationListItem(selectedType = selectedNotificationType, requiredType = NotificationType.TEN_MINUTES_BEFORE))
+        listItems.add(createNotificationListItem(selectedType = selectedNotificationType, requiredType = NotificationType.FIFTEEN_MINUTES_BEFORE))
+        listItems.add(createNotificationListItem(selectedType = selectedNotificationType, requiredType = NotificationType.THIRTY_MINUTES_BEFORE))
+        listItems.add(createNotificationListItem(selectedType = selectedNotificationType, requiredType = NotificationType.ONE_HOUR_BEFORE))
+        listItems.add(createNotificationListItem(selectedType = selectedNotificationType, requiredType = NotificationType.TWO_HOURS_BEFORE))
+        listItems.add(createNotificationListItem(selectedType = selectedNotificationType, requiredType = NotificationType.ONE_DAY_BEFORE))
         return listItems
     }
 
-    private fun createNotificationListItem(selectedId: Int, notificationId: Int): ListItem {
-        var title = ""
-        when (notificationId) {
-            NotificationIds.NO_NOTIFICATION -> {
-                title = ResourcesUtils.getString(R.string.notification_none)
-            }
-            NotificationIds.AT_TASK_TIME_NOTIFICATION -> {
-                title = ResourcesUtils.getString(R.string.notification_task_time)
-            }
-            NotificationIds.FIVE_MINUTES_BEFORE_NOTIFICATION -> {
-                title = ResourcesUtils.getString(R.string.notification_5_before)
-            }
-            NotificationIds.TEN_MINUTES_BEFORE_NOTIFICATION -> {
-                title = ResourcesUtils.getString(R.string.notification_10_before)
-            }
-            NotificationIds.FIFTEEN_MINUTES_BEFORE_NOTIFICATION -> {
-                title = ResourcesUtils.getString(R.string.notification_15_before)
-            }
-            NotificationIds.THIRTY_MINUTES_BEFORE_NOTIFICATION -> {
-                title = ResourcesUtils.getString(R.string.notification_30_before)
-            }
-            NotificationIds.ONE_HOUR_BEFORE_NOTIFICATION -> {
-                title = ResourcesUtils.getString(R.string.notification_1_hour_before)
-            }
-            NotificationIds.TWO_HOURS_BEFORE_NOTIFICATION -> {
-                title = ResourcesUtils.getString(R.string.notification_2_hour_before)
-            }
-            NotificationIds.ONE_DAY_BEFORE_NOTIFICATION -> {
-                title = ResourcesUtils.getString(R.string.notification_1_day_before)
-            }
-        }
-
+    private fun createNotificationListItem(selectedType: NotificationType, requiredType: NotificationType): ListItem {
         val data = ChoiceData(
-            id = notificationId,
-            title = title,
-            isSelected = notificationId == selectedId
+            id = requiredType.typeId,
+            title = ResourcesUtils.getString(requiredType.resourceId),
+            isSelected = requiredType == selectedType
         )
 
         return ListItem(data = data)
