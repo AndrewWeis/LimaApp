47ee2854dacb4e2555da2593da7ef4a790b78f13
i.chiesov <i.chiesov@gmail.com>
2022-05-06 13:46:15 +0700

    Move channel creation to app.

--- Android/Lima/app/src/main/java/start/up/tracker/application/App.kt
+++ Android/Lima/app/src/main/java/start/up/tracker/application/App.kt
@@ -2,8 +2,13 @@ package start.up.tracker.application
 
 import android.annotation.SuppressLint
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
+import start.up.tracker.R
 
 @HiltAndroidApp
 class App : Application() {
@@ -11,10 +16,37 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         context = applicationContext
+
+        createChannel(
+            getString(R.string.notification_channel_id),
+            getString(R.string.notification_channel_name)
+        )
     }
 
     companion object {
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                    setShowBadge(false)
+                }
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = getString(R.string.notification_channel_description)
+
+            val manager = getSystemService(
+                NotificationManager::class.java
+            )
+            manager.createNotificationChannel(notificationChannel)
+        }
     }
 }
